cc350ade79936e97596acd49e8524e8e018f9257
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2022-11-05 20:19:07 +0300

    feat: add 04-copy-directory task code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const fs = require('fs/promises');
 
+async function copy(){
+    try {
+
+        await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+
+        const files =  await fs.readdir(path.join(__dirname, 'files'));
+      
+        for (const file of files) {
+            let src = path.join(__dirname, 'files', file);
+            let dst = path.join(__dirname, 'files-copy', file);
+
+            await fs.copyFile(src, dst);
+
+            console.log(`- copied ${src} to ${dst}`);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+copy();
