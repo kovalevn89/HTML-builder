c9f7f6ab41d1e255f73a5829efb444c0f6e6687f
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2022-11-07 23:25:10 +0300

    fix: delete directory if exist in 04-copy-directory task code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,6 +4,7 @@ const fs = require('fs/promises');
 async function copy(){
     try {
 
+        await fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true });
         await fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
 
         const files =  await fs.readdir(path.join(__dirname, 'files'));
